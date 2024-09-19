4ac66ef6dc7c36b32eabdb9347f02e370f0c4bc6
jglee0814 <jglee0814@skku.edu>
2024-09-19 14:47:47 +0900

    refactor: board_id into id

--- src/main/java/com/mm/toy/domain/board/Board.java
+++ src/main/java/com/mm/toy/domain/board/Board.java
@@ -15,10 +15,10 @@ public class Board {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column
     @Id
+    @Column(name = "board_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long board_id;
+    private Long id;
 
     @Column
     private String title;
