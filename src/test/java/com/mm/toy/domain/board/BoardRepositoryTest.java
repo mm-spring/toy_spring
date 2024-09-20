ba8f658b20aa7d699e789bdbca8e5da467037fd9
hann <wjdgks1233@gmail.com>
2024-09-20 14:42:53 +0900

    [test/#4] add test class of repo & database clean up class

---
+++ src/test/java/com/mm/toy/domain/board/BoardRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.mm.toy.domain.board;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class BoardRepositoryTest {
+
+}
---
+++ src/test/java/com/mm/toy/domain/user/UserRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.mm.toy.domain.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+}
