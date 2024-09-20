33abd6bf4c4f0c30151b99bcdc3efeea9b1097ec
hann <wjdgks1233@gmail.com>
2024-09-20 15:25:02 +0900

    [Test/#4] findByName

--- src/main/java/com/mm/toy/domain/user/UserRepository.java
+++ src/main/java/com/mm/toy/domain/user/UserRepository.java
@@ -2,6 +2,9 @@ package com.mm.toy.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
     // save(entity), findById, findAll, delete, deleteAll, findOne
 }
--- src/test/java/com/mm/toy/domain/user/UserRepositoryTest.java
+++ src/test/java/com/mm/toy/domain/user/UserRepositoryTest.java
@@ -107,7 +107,32 @@ class UserRepositoryTest {
     @Test
     @DisplayName("유저를 생성 뒤 이름을 통해 유저 리스트를 조회합니다.")
     public void getUsersByName() {
-
+        //given
+        User user1 = User.builder()
+                .email("email1")
+                .role("role1")
+                .name("name1")
+                .password("password1")
+                .build();
+        User user2 = User.builder()
+                .email("email2")
+                .role("role2")
+                .name("name2")
+                .password("password2")
+                .build();
+        User user3 = User.builder()
+                .email("email3")
+                .role("role3")
+                .name("name1")
+                .password("password3")
+                .build();
+        List<User> users = userRepository.saveAll(List.of(user1, user2, user3));
+        //when
+        List<User> findUsers = userRepository.findByName(users.get(0).getName());
+        //then
+        assertThat(findUsers.size()).isEqualTo(2);
+        assertThat(findUsers).anyMatch(user -> user.getPassword().equals(user1.getPassword()));
+        assertThat(findUsers).anyMatch(user -> user.getPassword().equals(user3.getPassword()));
     }
 
 }
