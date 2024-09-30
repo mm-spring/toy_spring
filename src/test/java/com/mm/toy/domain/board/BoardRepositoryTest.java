cecc4861dec1abaef2ae803486f648815ca0f6a8
jglee0814 <jglee0814@skku.edu>
2024-09-30 12:18:07 +0900

    Add necessary components

--- src/test/java/com/mm/toy/domain/board/BoardRepositoryTest.java
+++ src/test/java/com/mm/toy/domain/board/BoardRepositoryTest.java
@@ -1,12 +1,19 @@
 package com.mm.toy.domain.board;
 
+import com.mm.toy.domain.user.User;
 import com.mm.toy.domain.user.UserRepository;
 import com.mm.toy.global.service.DatabaseCleanup;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
 @SpringBootTest
 class BoardRepositoryTest {
 
@@ -16,6 +23,27 @@ class BoardRepositoryTest {
     UserRepository userRepository;
     @Autowired
     DatabaseCleanup databaseCleanup;
+
+    User user1;
+    User user2;
+
+    @BeforeEach
+    void setUp(){
+        user1 = User.builder()
+                .username("username1")
+                .password("password1")
+                .email("eamil1")
+                .name("name1")
+                .build();
+
+        user2 = User.builder()
+                .username("username2")
+                .password("password2")
+                .email("eamil2")
+                .name("name2")
+                .build();
+        userRepository.saveAll(List.of(user1, user2));
+    }
 
     @AfterEach
     void cleanUp() {
