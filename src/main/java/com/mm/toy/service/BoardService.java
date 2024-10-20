9df1cc72077fff6d1a25616c5f80d97341979b3f
jglee0814 <jglee0814@skku.edu>
2024-10-20 20:17:02 +0900

    [feat/#10] Create writeBoard

---
+++ src/main/java/com/mm/toy/service/BoardService.java
@@ -0,0 +1,30 @@
+package com.mm.toy.service;
+
+import com.mm.toy.domain.board.Board;
+import com.mm.toy.domain.board.BoardRepository;
+import com.mm.toy.domain.board.BoardRequestDto;
+import com.mm.toy.domain.user.User;
+import com.mm.toy.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private BoardRepository boardRepository;
+    private UserService userService;
+
+    @Transactional
+    public Long writeBoard(String username, BoardRequestDto boardRequestDto){
+        Board board = Board.builder()
+                .title(boardRequestDto.getTitle())
+                .content(boardRequestDto.getContent())
+                .build();
+        Board savedBoard = boardRepository.save(board);
+        User user = userService.getUserInfoByUsername(username);
+        user.addBoard(savedBoard);
+        return savedBoard.getId();
+    }
+
+}
