907637d1e188fb65fbd979396a8d47037fb9fe68
hann <wjdgks1233@gmail.com>
2024-11-05 22:33:14 +0900

    [Feat/#19] create signup.html

--- src/main/java/com/mm/toy/controller/UserController.java
+++ src/main/java/com/mm/toy/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.mm.toy.controller;
 
+import com.mm.toy.Dto.UserRegisterDto;
 import com.mm.toy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -11,9 +14,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/user/register")
-    public String userRegister() {
+    @GetMapping("/signup")
+    public String showSignUpForm(Model model) {
+        model.addAttribute("userRegisterDto", new UserRegisterDto());
+        return "signup";
+    }
 
+    @PostMapping("/signup")
+    public String registerUser(@ModelAttribute UserRegisterDto userRegisterDto) {
+        userService.registerUser(userRegisterDto);
+        return "redirect:/";
     }
 
     @GetMapping("/user/{username}")
