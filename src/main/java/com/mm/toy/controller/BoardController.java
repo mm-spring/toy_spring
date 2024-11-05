cc2a4747c4481daa99045bf898894a9b18275b9c
hann <wjdgks1233@gmail.com>
2024-11-05 22:14:25 +0900

    [Feat/#19] set method of BoardController

--- src/main/java/com/mm/toy/controller/BoardController.java
+++ src/main/java/com/mm/toy/controller/BoardController.java
@@ -1,4 +1,44 @@
 package com.mm.toy.controller;
 
+import com.mm.toy.service.BoardService;
+import com.mm.toy.service.CommentService;
+import com.mm.toy.service.LikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class BoardController {
+    private final BoardService boardService;
+    private final LikeService likeService;
+    private final CommentService commentService;
+
+    @GetMapping("/boards")
+    public String getBoardList() {
+
+    }
+
+    @GetMapping("/boards/{boardId}")
+    public String getBoardInfo() {
+
+    }
+
+    @PostMapping("/boards/write")
+    public String writeBoard() {
+
+    }
+
+    @PutMapping("/boards/edit/{boardId}")
+    public String editBoard() {
+
+    }
+
+    @DeleteMapping("/boards/delete/{boardId}")
+    public String deleteBoard() {
+
+    }
 }
