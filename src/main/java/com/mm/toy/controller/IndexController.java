228136f70d46fa89f9ae050c88599da76616271d
hann <wjdgks1233@gmail.com>
2024-11-05 22:27:04 +0900

    [Feat/#19] index.html

--- src/main/java/com/mm/toy/controller/IndexController.java
+++ src/main/java/com/mm/toy/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.mm.toy.controller;
 
-import ch.qos.logback.core.model.Model;
+import com.mm.toy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
+    private final UserService userService;
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("users", userService.getAllUser());
+        return "home";
+    }
     @GetMapping("/")
     public String index(Model model) {
 //        model.addAttribute("posts", postsService.findAllDesc());
