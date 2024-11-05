3b6abe408febffa1184e94b1e6aa8f7b58e7e9c0
hann <wjdgks1233@gmail.com>
2024-11-05 22:54:00 +0900

    [Feat/#19] add getBoardlist method in BoardController

--- src/main/java/com/mm/toy/Dto/BoardDto.java
+++ src/main/java/com/mm/toy/Dto/BoardDto.java
@@ -1,13 +1,17 @@
 package com.mm.toy.Dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class BoardDto {
     private String title;
     private String content;
+    private int likeCount;
+    private int commentCount;
 }
--- src/main/java/com/mm/toy/controller/BoardController.java
+++ src/main/java/com/mm/toy/controller/BoardController.java
@@ -1,21 +1,43 @@
 package com.mm.toy.controller;
 
+import com.mm.toy.Dto.BoardDto;
+import com.mm.toy.domain.Board;
+import com.mm.toy.domain.User;
 import com.mm.toy.service.BoardService;
 import com.mm.toy.service.CommentService;
 import com.mm.toy.service.LikeService;
+import com.mm.toy.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
+    private final UserService userService;
     private final BoardService boardService;
     private final LikeService likeService;
     private final CommentService commentService;
 
+    @GetMapping("/user/{userId}/boards")
+    public String getBoardList(@PathVariable Long userId, Model model) {
+        User user = userService.getUserInfoById(userId);
+        List<Board> boards = boardService.getBoardsByUsername(user.getUsername());
+        List<BoardDto> collect = boards.stream().map(board -> toDto(board)).collect(Collectors.toList());
+        model.addAttribute("boards", collect);
+        return "boardList";
+    }
 
+    private BoardDto toDto(Board board) {
+        return BoardDto.builder()
+                .title(board.getTitle())
+                .content(board.getContent())
+                .commentCount(board.getComments().size())
+                .likeCount(likeService.countLike(board.getId()))
+                .build();
+    }
 }
