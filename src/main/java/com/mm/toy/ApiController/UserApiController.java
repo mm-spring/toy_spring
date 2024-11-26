78ce35aa8c5b26ec72e25343fff85578ea1dd1ae
jglee0814 <jglee0814@skku.edu>
2024-11-26 16:46:17 +0900

    [feat/#22] Update signUp

--- src/main/java/com/mm/toy/ApiController/UserApiController.java
+++ src/main/java/com/mm/toy/ApiController/UserApiController.java
@@ -1,9 +1,20 @@
 package com.mm.toy.ApiController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mm.toy.Dto.UserRegisterDto;
+import com.mm.toy.domain.User;
+import com.mm.toy.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("signup")
+    public Long signUp(@RequestBody UserRegisterDto userRegisterDto) {
+        return userService.registerUser(userRegisterDto);
+    }
 }
