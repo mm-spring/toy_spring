ad6290e302e9a40e0b867fce46c30247e57c9ae7
jglee0814 <jglee0814@skku.edu>
2024-11-26 17:55:35 +0900

    [feat/#22] Create BoardApiController

---
+++ src/main/java/com/mm/toy/ApiController/BoardApiController.java
@@ -0,0 +1,11 @@
+package com.mm.toy.ApiController;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/boards")
+public class BoardApiController {
+}
