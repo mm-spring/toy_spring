7fddc500a781782ba87bbe968449f65b0823acb4
jglee0814 <jglee0814@skku.edu>
2024-11-26 18:35:41 +0900

    [feat/#22] Update writeBoard

--- src/main/java/com/mm/toy/ApiController/BoardApiController.java
+++ src/main/java/com/mm/toy/ApiController/BoardApiController.java
@@ -1,11 +1,21 @@
 package com.mm.toy.ApiController;
 
+import com.mm.toy.Dto.BoardRequestDto;
+import com.mm.toy.domain.User;
+import com.mm.toy.service.BoardService;
+import com.mm.toy.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/boards")
 public class BoardApiController {
+    private final BoardService boardService;
+    private final UserService userService;
+
+    @PostMapping("/write-board")
+    public Long writeBoard(@RequestParam String username, @RequestBody BoardRequestDto boardRequestDto){
+        return boardService.writeBoard(username, boardRequestDto);
+    }
 }
