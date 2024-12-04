8e4a69099a00273c42af867d58ac54e865756bb3
jglee0814 <jglee0814@skku.edu>
2024-12-04 21:46:55 +0900

    [feat/#22] Update likeBoard

--- src/main/java/com/mm/toy/ApiController/LikeApiController.java
+++ src/main/java/com/mm/toy/ApiController/LikeApiController.java
@@ -2,12 +2,16 @@ package com.mm.toy.ApiController;
 
 import com.mm.toy.service.LikeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/like")
 public class LikeApiController {
     private final LikeService likeService;
+
+    @PostMapping("boards/{boardId}")
+    public Long likeBoard(@PathVariable Long boardId, @RequestParam String username) {
+        return likeService.likeBoard(username, boardId);
+    }
 }
