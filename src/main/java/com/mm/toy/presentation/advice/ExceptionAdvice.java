5fc3dd52f2d56796e0d0b1575f4630c96c1b19be
jglee0814 <jglee0814@skku.edu>
2024-12-28 17:46:43 +0900

    [#28/Feature] Create GeneralException

---
+++ src/main/java/com/mm/toy/presentation/advice/ExceptionAdvice.java
@@ -0,0 +1,14 @@
+package com.mm.toy.presentation.advice;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object>
+}
